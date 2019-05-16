3dbbe3da410f0938bef5ce0652bd4fe5d99e9beb
ngxson-sp <thichthat@gmail.com>
2019-05-17 00:41:56 +0200

    init

---
+++ js/nui.js
@@ -0,0 +1,60 @@
+
+(function ($) {
+    var model = null
+
+    async function init() {
+        model = await tf.loadLayersModel('model/model.json')
+    }
+
+    async function predict(vec) {
+        var result = model.predict(tf.tensor([vec]))
+        result = await result.array()
+        result = result[0]
+        return result[0] > result[1] ? 'Nam' : 'Nữ'
+    }
+
+    function toVector(name) {
+        var arr = name.toLowerCase().split(' ')
+        var vec = arr.map(word => {
+            return TOKEN[word] || 0
+        })
+        while (vec.length < 6) {
+            vec.unshift(0)
+        }
+        return vec
+    }
+
+    function onClickPredict() {
+        $('#result').html(`Xin chờ...`)
+        var name = $('#input-name').val()
+        var vec = toVector(name)
+        predict(vec).then(gender => {
+            $('#result').html(`
+                Tôi xin mạnh dạn đoán:
+                <br/>
+                Giới tính của ${toTitleCase(name)} là: <b>${gender}</b>
+                <br/><br/>
+            `)
+        })
+    }
+
+    $('#predict').click(function() {
+        requestAnimationFrame(() => $('#result').html(`Xin chờ...`))
+        setTimeout(onClickPredict, 50)
+    })
+
+    $('form').submit(function(event) {
+        event.preventDefault()
+        requestAnimationFrame(() => $('#result').html(`Xin chờ...`))
+        setTimeout(onClickPredict, 50)
+    })
+
+    init()
+})(jQuery);
+
+function toTitleCase(str) {
+    return str.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ')
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+### tfjs-vn-name-to-gender
+
+This is a small project using `tensorflowjs`
+
+The goal is to make a small app to predict the gender base on a person's name. It currently works only on vietnamese names. The accuracy of the trained model is 97%
+
+### How it's made
+
+1. Generate `dataset.csv` containing Name - Gender
+2. Train the model using tensorflow (keras) - More details in `LSTM.ipynb`
+3. Export the model to `model` folder (in JSON format)
+4. Import it into `tensorflowjs` on web
+
+### Author
+
+* ngxson (Nui Nguyen)
+* Email: contact at ngxson dot com
+* My website: https://ngxson.com
